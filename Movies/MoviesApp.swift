894523b6c5724857940d14cc41b3e8aa4f28d091
Kostas Karayannis <kkarayannis@gmail.com>
2024-05-03 20:42:38 +0200

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Konstadinos Karayannis on 03/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
